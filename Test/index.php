d91db42c8a7d6486a1d33c7fb0d7e7e9f4df0d84
karimul2001 <karimul.bd501@gmail.com>
2025-10-22 09:25:04 +0600

    first commit

---
+++ Test/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+    <h1>Welcome to PHP</h1>
+    <?php 
+    
+    
+    print "Hello World! <h1>I am Karimul Islam</h1>"
+    
+    
+    ?>
+
+<?php 
+
+echo "how problem."
+
+?>
+
+<?php
+
+print " php is the most important . She will never be able to know"
+
+?>
+
+
+</body>
+</html>
