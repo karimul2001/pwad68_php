d332f2a5e1eb3067a6095c1e6a96fa05149c89d5
karimul2001 <karimul.bd501@gmail.com>
2025-11-20 13:09:48 +0600

    20 Nov php Project

---
+++ Project/admin/inc/db_config.php
@@ -0,0 +1,9 @@
+<?php 
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "pwad68_test1";
+$db = new mysqli($host, $user, $password, $database);
+$admin_url = "http://localhost/PWAD-68/Project/admin/";
+
+?>
